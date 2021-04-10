b53e8d0b941df52f73f31940eb03873e280edf70
RajapandiR <Rajapandibsc12@gmail.com>
2021-04-10 10:34:41 +0530

    Server updated

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
