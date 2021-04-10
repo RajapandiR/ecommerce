35207e23a1a9e63c9ff567e28c00e731c75590ca
RajapandiR <Rajapandibsc12@gmail.com>
2021-04-10 10:43:21 +0530

    Server updated

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #DJANGO WHITENOISE CONFIGURATION
 ]
 
 ROOT_URLCONF = 'ecomproject.urls'
