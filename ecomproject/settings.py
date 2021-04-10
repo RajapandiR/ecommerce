f6e205cd4b1968f91e9965be85d594388b4e8b1a
RajapandiR <Rajapandibsc12@gmail.com>
2021-04-10 12:25:52 +0530

    Server updated

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -139,13 +139,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_URL = '/image/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
 
 AUTH_USER_MODEL ='ecomapp.User'
