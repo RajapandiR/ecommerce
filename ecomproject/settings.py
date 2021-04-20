7e338a4888107dda32a6c9b6f1e150870ffdee76
RajapandiR <Rajapandibsc12@gmail.com>
2021-04-20 13:11:32 +0530

    Bug Fixing

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "sm", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
